597a63d10d25efacf84d33b69a27f1d27172b42a
YiMac <li_xianghao@YiMac2020.local>
2021-08-30 16:14:15 +0900

    Updated

--- kr/assets/js/iamport.js
+++ kr/assets/js/iamport.js
@@ -21,28 +21,19 @@ function pay() {
   }
 
   IMP.request_pay({
-    pg : 'kakaopay',
-    pay_method : 'card',
-    merchant_uid : 'merchant_' + new Date().getTime(),
-    name : '주문명:결제테스트',
-    amount : 14000,
-    buyer_email : 'iamport@siot.do',
-    buyer_name : '구매자이름',
-    buyer_tel : '010-1234-5678',
-    buyer_addr : '서울특별시 강남구 삼성동',
-    buyer_postcode : '123-456'
-    // amount : 10,
-    // // amount : product_amount,
-  	// buyer_name : '게스트(비회원)',
+    amount : 10000,
+    // amount : product_amount,
+  	buyer_name : '게스트(비회원)',
+    name : '상품(다운로드)',
   	// name : product_name
-    // // pg: 'inicis', // version 1.1.0부터 지원.
-    // // pay_method: 'card',
-    // // merchant_uid: 'merchant_' + new Date().getTime(),
-    // // buyer_email: 'iamport@siot.do',
-    // // buyer_tel: '010-1234-5678',
-    // // buyer_addr: '서울특별시 강남구 삼성동',
-    // // buyer_postcode: '123-456',
-    // // m_redirect_url: 'https://www.yourdomain.com/payments/complete'
+    // pg: 'kakaopay',
+    // pay_method: 'card',
+    // merchant_uid: 'merchant_' + new Date().getTime(),
+    // buyer_email: 'iamport@siot.do',
+    // buyer_tel: '010-1234-5678',
+    // buyer_addr: '서울특별시 강남구 삼성동',
+    // buyer_postcode: '123-456',
+    // m_redirect_url: 'https://www.yourdomain.com/payments/complete'
   }, function(rsp) {
     if ( rsp.success ) {
       //[1] 서버단에서 결제정보 조회를 위해 jQuery ajax로 imp_uid 전달하기
